aaebf56ac9794f64c0953f683814883fd7eb91bb
CarlosReyesStoneham <cer26@duke.edu>
2013-11-08 15:42:51 -0500

    weapons improvements

--- src/gameObject/item/Weapon.java
+++ src/gameObject/item/Weapon.java
@@ -7,9 +7,25 @@ import java.util.List;
 import java.util.Map;
 
 
+/**
+ * Weapon is any item that modifies stat modifiers
+ * and by extention stats, the primary difference between a weapon and
+ * armor is that armor does not have actions, whereas weapons do. Ex.
+ * a weapon such as a sword can have an action called "stab" that does some damage
+ * to an opponent.
+ * 
+ * @author carlosreyes
+ * 
+ */
 public class Weapon extends Equipment {
     private Map<String, Integer> myStatMap = new HashMap<String, Integer>();
     private List<CombatAction> myActionList;
+
+    public Weapon (String name, List<CombatAction> actionList, Map<String, Integer> statMap) {
+        super.setName(name);
+        myStatMap = statMap;
+        myActionList = actionList;
+    }
 
     @Override
     public void statEffect (GameUnit unit) {
@@ -19,6 +35,12 @@ public class Weapon extends Equipment {
         }
     }
 
+    /**
+     * Select action takes in a given action and returns it from the action list.
+     * 
+     * @param action
+     * @return
+     */
     public CombatAction selectAction (CombatAction action) {
         for (CombatAction a : myActionList) {
             if (a.equals(action)) { return a; }
