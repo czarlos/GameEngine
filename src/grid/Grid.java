e761451573d5a37f8f5481e7ba810f067dff7040
kjian279 <kjian279@gmail.com>
2013-11-10 23:55:26 -0500

    adding movement methods to grid and commenting

--- src/grid/Grid.java
+++ src/grid/Grid.java
@@ -9,7 +9,11 @@ import java.util.Map;
 import java.util.Map.Entry;
 import view.Drawable;
 
-
+/**
+ * Grid class. Holds all tiles and objects, calculates movement
+ * @author Kevin, Ken
+ *
+ */
 public class Grid implements Drawable {
     private int myWidth;
     private int myHeight;
@@ -19,6 +23,11 @@ public class Grid implements Drawable {
                                                            // passes, 1 = everything passes. Put
                                                            // this map in stage controller?
 
+    /**
+     * Creates a grid with the width and height set
+     * @param width - int of number of columns of tiles
+     * @param height - int of number of rows of tiles
+     */
     public Grid (int width, int height) {
         myWidth = width;
         myHeight = height;
@@ -29,11 +38,17 @@ public class Grid implements Drawable {
         initGrid();
     }
 
+    /**
+     * Sets up default grid with tiles and objects
+     */
     private void initGrid () {
         initTiles();
         testInitObjects();
     }
 
+    /**
+     * Creates default tiles for grid
+     */
     private void initTiles () {
         for (int i = 0; i < myWidth; i++) {
             for (int j = 0; j < myHeight; j++) {
@@ -42,13 +57,30 @@ public class Grid implements Drawable {
         }
     }
     
+    /**
+     * Creates default objects and units for grid
+     */
     private void testInitObjects () {
         myObjects.put(new Coordinate(3, 5),  new GameObject());
-        GameUnit link = new GameUnit();
-        myObjects.put(new Coordinate(6, 3), link);
-        findMovementRange(new Coordinate(6, 3), link.getStats().getStatValue("movement"), link);
+        GameObject link = new GameUnit();
+        myObjects.put(new Coordinate(4, 5), link);
+        findMovementRange(new Coordinate(4, 5), ((GameUnit) link).getStats().getStatValue("movement"), link);
     }
     
+    /**
+     * Initiates the moving process for a gameUnit
+     * @param gameUnit - GameUnit that is being moved
+     */
+    public void move (GameUnit gameUnit) {
+        
+    }
+    
+    /**
+     * Sets the tiles active that the GameObject can move to
+     * @param coordinate - Coodinate of the current position of the GameObject
+     * @param range - int of range that the GameObject can move
+     * @param gameObject - 
+     */
     private void findMovementRange (Coordinate coordinate, int range, GameObject gameObject) {
         int[] rdelta = {-1, 0, 0, 1};
         int[] cdelta = {0, -1, 1, 0};
@@ -72,8 +104,24 @@ public class Grid implements Drawable {
         
     }
     
+    /**
+     * Checks if the input coordinate is on the grid
+     * @param x - int of x coordinate
+     * @param y - int of y coordinate
+     * @return - boolean of if the coordinate is valid
+     */
     private boolean onGrid (int x, int y) {
         return (0 <= x && x < myWidth && 0 <= y && y < myHeight);
+    }
+    
+    /**
+     * Checks if a coordinate is a valid move (the tile is active)
+     * @param x - int of x coordinate
+     * @param y - int of y coordinate
+     * @return - boolean of if the coordinate is valid
+     */
+    public boolean canMove (int x, int y) {
+        return getTile(x, y).isActive();
     }
 
     public GameObject getObject (int x, int y) {
