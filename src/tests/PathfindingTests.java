a3a3422b24784d8ae2d1e7513ac67ef77483b564
CarlosReyesStoneham <cer26@duke.edu>
2013-11-14 23:30:38 -0500

    added pathfinding test

---
+++ src/tests/PathfindingTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PathfindingTests {
+
+    @Test
+    public void test () {
+        fail("Not yet implemented");
+    }
+
+}
