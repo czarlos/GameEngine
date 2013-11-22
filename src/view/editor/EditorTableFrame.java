01a1d58e15783773a593f67addd1198b157a1d0d
Chris Murphy <cjm53@duke.edu>
2013-11-21 19:36:33 -0500

    <gui> started connecting jtables with stageeditorpanels

---
+++ src/view/editor/EditorTableFrame.java
@@ -0,0 +1,46 @@
+package view.editor;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+
+public class EditorTableFrame extends JFrame implements WindowListener{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7225370093723510786L;
+
+    private StageEditorPanel myParent;
+    private String myType;
+    
+    public EditorTableFrame(String type, StageEditorPanel stage){
+        super(type + " Editor");
+        myParent = stage;
+        myType = type;
+    }
+    
+    @Override
+    public void windowOpened (WindowEvent e) {}
+
+    @Override
+    public void windowClosing (WindowEvent e) {
+        myParent.refreshTab(myType);
+    }
+
+    @Override
+    public void windowClosed (WindowEvent e) {}
+
+    @Override
+    public void windowIconified (WindowEvent e) {}
+
+    @Override
+    public void windowDeiconified (WindowEvent e) {}
+
+    @Override
+    public void windowActivated (WindowEvent e) {}
+
+    @Override
+    public void windowDeactivated (WindowEvent e) {}
+
+}
---
+++ src/view/editor/StageController.java
@@ -0,0 +1,16 @@
+package view.editor;
+
+
+public class StageController {
+    
+    private String activeObject;
+    
+    public StageController(){
+    }    
+    
+    private String getCurrentActiveObjectName(){
+        return activeObject;
+    }
+    
+    
+}
