e5e81d9d007af5e9382692f63e880662b3c5193a
kjian279 <kjian279@gmail.com>
2013-11-22 04:36:13 -0500

    <triv> testing

--- src/view/editor/EditorFrame.java
+++ src/view/editor/EditorFrame.java
@@ -1,17 +1,12 @@
 package view.editor;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
