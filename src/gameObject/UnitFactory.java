4edfe43a8268729f924ca0a38e08c02465d8079b
CarlosReyesStoneham <cer26@duke.edu>
2013-11-22 22:43:53 -0500

    <combat> factories can make many units, time to make units associated with a set num of turns

--- src/gameObject/UnitFactory.java
+++ src/gameObject/UnitFactory.java
@@ -4,13 +4,14 @@ import grid.Coordinate;
 import grid.Grid;
 import java.util.List;
 import java.util.Map;
+import team.Team;
 
 
 /**
  * Factories produce units which are placed on the grid for the
  * user to use when they are finished. Factories place units to
  * the side of the factory, only one unit can be in this place
- * at a time. When a user is on this tile it will lock the factory.
+ * at a time.
  * 
  * @author carlosreyes
  * 
@@ -19,7 +20,6 @@ public class UnitFactory extends GameObject {
 
     private Map<String, GameUnit> myPossibleUnits;
     private Map<String, Integer> myNumberOfTurns;
-    private int myCounter = 0;
     private Map<GameUnit, Integer> myPendingUnits;
     private Grid myGrid;
     private Coordinate myLocation;
@@ -27,22 +27,6 @@ public class UnitFactory extends GameObject {
     public UnitFactory (Map<String, GameUnit> possibleUnits, Grid grid, Coordinate location) {
         myPossibleUnits = possibleUnits;
         myLocation = location;
-    }
-
-    /**
-     * Makes new unit from the possible units and puts it into your list of units,
-     * gives the unit a generic name + a number to distinguish it from other similar units.
-     * Makes units and puts them at the same grid position as the factory.
-     * 
-     * @param unitName
-     * @param teamUnitList
-     */
-    public void produce (String unitName, List<GameUnit> unitList) {
-        GameUnit unit = myPossibleUnits.get(unitName);
-        unit.setName(unitName + myCounter);
-        myGrid.placeObject(validLocation(unit), unit);
-        unitList.add(unit);
-        myCounter++;
     }
     
     /**
@@ -63,16 +47,37 @@ public class UnitFactory extends GameObject {
     }
     
     /**
-     * Checks through the pending units list to see if any units have finished completing
-     * if they have, calls produce to make the unit that has finished.
-     * @param unitName - The unique name of the unit being made
-     * @param teamUnitList - The place where units will be placed
+     * Loops through all of the units pending creation, if
+     * the amount of turns that it takes for that unit to be
+     * made has been met, this unit is put in the unitList of
+     * the team that is responsible for creating said unit.
+     * Units are placed at a valid tile adjacent to the factory.
+     * If a unit is not being created the turn counter on that
+     * unit is decremented. Should be called every turn.
+     * 
+     * @param team - The team associated with this factory
      */
-    public void makeUnit (String unitName, List<GameUnit> unitList) {
+    public void makeUnit (Team team) {
         for (GameUnit unit : myPendingUnits.keySet()) {
             if (myPendingUnits.get(unit) == 0) {
-                produce(unitName, unitList);
+                myPendingUnits.remove(unit);
+                team.getGameUnits().add(unit);
+                myGrid.placeObject(validLocation(unit), unit);
+            }
+            else {
+                myPendingUnits.put(unit, myPendingUnits.get(unit)-1);
             }
         }
+    }
+    
+    /**
+     * Adds the desired unit to be created to the map of the units
+     * pending creation along with the number of turns it takes to
+     * make this unit.
+     * 
+     * @param unitName - The string name of the unit to be created
+     */
+    public void enqueueUnit (String unitName) {
+        myPendingUnits.put(myPossibleUnits.get(unitName), myNumberOfTurns.get(unitName));
     }
 }
