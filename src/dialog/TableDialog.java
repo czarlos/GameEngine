806f531d03c13d7cad810c72abfc7790d9dad34d
Leeviana Gray <leeviana@gmail.com>
2013-11-25 02:21:12 -0500

    <triv> started messing with table stuff

--- src/dialog/TableDialog.java
+++ src/dialog/TableDialog.java
@@ -1,15 +1,21 @@
 package dialog;
 
 import gameObject.Stats;
+import grid.Tile;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import view.Customizable;
 
 
 public class TableDialog extends JDialog {
@@ -18,6 +24,23 @@ public class TableDialog extends JDialog {
     public TableDialog (GameTableModel model, ActionListener okListener) {
         addTable(model);
         myModel = model;
+        
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+
+        JButton addNewButton = new JButton("add new");
+
+        addNewButton.addActionListener(new AddNewListener(model));
+
+        JButton ok = new JButton("Save");
+        ok.addActionListener(okListener);
+
+        buttonPanel.add(ok);
+        buttonPanel.add(addNewButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        pack();
     }
 
     private void addTable (GameTableModel model) {
@@ -49,6 +72,34 @@ public class TableDialog extends JDialog {
 
     }
 
+    private class AddNewListener implements ActionListener {
+
+        private JDialog dialog;
+        private ImageCreator imageCreator;
+        private GameTableModel myModel;
+
+        public AddNewListener (GameTableModel model) {
+            super();
+            myModel = model;
+        }
+
+        public void actionPerformed (ActionEvent e) {
+            ArrayList<Customizable> newList = new ArrayList<Customizable>();
+            Tile defTile = new Tile();
+            defTile.setName("New Tile");
+            defTile.setImageAndPath("resources/grass.png");
+            defTile.setMoveCost(1);
+
+            Stats s = new Stats();
+            s.setStats(new HashMap<String, Integer>());
+
+            defTile.setStats(s);
+
+            newList.add(defTile);
+            myModel.addPreviouslyDefined(newList);
+        }
+    }
+    
     protected class DefaultCancelListener implements ActionListener {
 
         private JDialog dialog;
