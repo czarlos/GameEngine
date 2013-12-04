cbd7a7b0fa4677f3fd2b6a3a375a6aa4404baf48
Andrew Bradshaw <wiggin315@gmail.com>
2013-12-04 18:33:10 -0500

    <combat> reworked outcomes and added getter/setters to actions and outcomes

--- src/gameObject/action/Outcome.java
+++ src/gameObject/action/Outcome.java
@@ -1,18 +1,21 @@
 package gameObject.action;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import gameObject.GameUnit;
 
 
-public abstract class Outcome {
-    protected String myType;
-    protected String myName;
-    protected int myAmount;
+public class Outcome {
+    private String myType;
+    private String myName;
+    private int myAmount;
+    private boolean isFixed;
 
-    public Outcome (String type, String name, int amount) {
-        myType = type;
+    public Outcome (String name, int amount, boolean fixed) {
         myName = name;
         myAmount = amount;
-    }
+        isFixed = fixed;
+    }    
 
     /**
      * applyOutcomes edits a GameUnit based on user specified type, name, and
@@ -24,7 +27,44 @@ public abstract class Outcome {
      * @param effectiveness
      *        - A measurement of how much of an outcome should occur
      */
-    public abstract void applyOutcome (GameUnit unit, double effectiveness);
+    public void applyOutcome (GameUnit unit, double effectiveness) {
+        
+        if (isStat(unit, myName)) {
+            myType = "Stat";
+        }
+        else {
+            myType = "Item";
+        }
+        
+        try {
+            Method get = unit.getClass().getDeclaredMethod("get" + myType,
+                                                           String.class);
+            Method set = unit.getClass().getDeclaredMethod("set" + myType,
+                                                           String.class, int.class);
+
+            int newAmount;
+
+            if (isFixed) {
+                newAmount = (int) get.invoke(unit, myName) + myAmount;
+            }
+            else {
+                newAmount = Math
+                        .round((float) ((int) get.invoke(unit, myName) + myAmount
+                                                                         * effectiveness));
+            }
+
+            newAmount = (newAmount > 0 ? newAmount : 0);
+
+            set.invoke(unit, myName, newAmount);
+
+        }
+        catch (NoSuchMethodException | SecurityException
+                | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            // TODO Figure out what to do
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Checks whether or not an outcome is legal
@@ -33,6 +73,69 @@ public abstract class Outcome {
      * @param effectiveness
      * @return true if legal, false otherwise
      */
-    public abstract boolean checkValidOutcome (GameUnit unit,
-                                               double effectiveness);
+    public boolean checkValidOutcome (GameUnit unit, double effectiveness) {
+        
+        if (isStat(unit, myName)) {
+            myType = "Stat";
+        }
+        else {
+            myType = "Item";
+        }
+        
+        try {
+            Method get = unit.getClass().getDeclaredMethod("get" + myType,
+                                                           String.class);
+
+            int oldAmount = (int) get.invoke(unit, myName);
+            int newAmount;
+
+            if (isFixed) {
+                newAmount = (int) get.invoke(unit, myName) + myAmount;
+            }
+            else {
+                newAmount = Math
+                        .round((float) ((int) get.invoke(unit, myName) + myAmount
+                                                                         * effectiveness));
+            }
+
+            if (oldAmount - newAmount < 0) { return false; }
+
+        }
+        catch (NoSuchMethodException | SecurityException
+                | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            // TODO Figure out what to do
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+    
+    private boolean isStat(GameUnit unit, String name) {
+        return unit.getStats().getStatNames().contains(name);
+    }
+
+    public String getName () {
+        return myName;
+    }
+
+    public void setName (String name) {
+        myName = name;
+    }
+
+    public int getAmount () {
+        return myAmount;
+    }
+
+    public void setAmount (int amount) {
+        myAmount = amount;
+    }
+
+    public boolean isFixed () {
+        return isFixed;
+    }
+
+    public void setIsFixed (boolean fixed) {
+        isFixed = fixed;
+    }
 }
