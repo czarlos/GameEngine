b67204fd8629e6b101b6d086ac2b8f403f40e847
Andrew Bradshaw <wiggin315@gmail.com>
2013-12-07 06:27:39 -0500

    <actions> reworked combatGetItemValue

--- src/tests/TradeItemTest.java
+++ src/tests/TradeItemTest.java
@@ -11,21 +11,27 @@ import org.junit.Test;
 public class TradeItemTest {
     GameUnit playerUnit;
     GameUnit allyUnit;
-    Item item;
+    Item item1;
+    Item item2;
+    Item item3;
+    Item item4;
     TradeAction ta;
     
     @Before
     public void setUp () throws Exception {
         // Generate Item
-        item = makeEmptyItem("item");
+        item1 = makeEmptyItem("item");
+        item2 = makeEmptyItem("shoe");
+        item3 = makeEmptyItem("pants");
+        item4 = makeEmptyItem("shirt");
         
         // Set Up Units to test
         playerUnit = new GameUnit();
         playerUnit.setAffiliation("player");
         allyUnit = new GameUnit();
         allyUnit.setAffiliation("player");
-        allyUnit.addItem(item);
-        allyUnit.addItem(item);
+        allyUnit.addItem(item1);
+        allyUnit.addItem(item1);
         
         // Set Up Trade Action
         ta = new TradeAction(allyUnit.getInteractions().get(0).split(" ")[1]);
@@ -33,8 +39,8 @@ public class TradeItemTest {
 
     @Test
     public void testPlayerBeforeTrade () {
-        assertTrue("Unit does not have 'item' in its set",!playerUnit.getItems().contains(item));
-        assertEquals(playerUnit.getItemAmount(item.getName()), 0);
+        assertTrue("Unit does not have 'item' in its set",!playerUnit.getItems().contains(item1));
+        assertEquals(playerUnit.getItemAmount(item1.getName()), 0);
         assertEquals(playerUnit.getItem("item"), null);
     }
     
@@ -42,24 +48,24 @@ public class TradeItemTest {
     public void testPlayerAfterTrade () {
         ta.doAction(playerUnit, allyUnit);
         
-        assertTrue("Unit does have 'item' in its set",playerUnit.getItems().contains(item));
-        assertEquals(playerUnit.getItemAmount(item.getName()), 2);
-        assertEquals(playerUnit.getItem("item"), item);
+        assertTrue("Unit does have 'item' in its set",playerUnit.getItems().contains(item1));
+        assertEquals(playerUnit.getItemAmount(item1.getName()), 2);
+        assertEquals(playerUnit.getItem("item"), item1);
     }
     
     @Test
     public void testAllyBeforeTrade () {
-        assertTrue("Unit does have 'item' in its set",allyUnit.getItems().contains(item));
-        assertEquals(allyUnit.getItemAmount(item.getName()), 2);
-        assertEquals(allyUnit.getItem("item"), item);
+        assertTrue("Unit does have 'item' in its set",allyUnit.getItems().contains(item1));
+        assertEquals(allyUnit.getItemAmount(item1.getName()), 2);
+        assertEquals(allyUnit.getItem("item"), item1);
     }
     
     @Test
     public void testAllyAfterTrade () {
         ta.doAction(playerUnit, allyUnit);
         
-        assertTrue("Unit does not have 'item' in its set",!allyUnit.getItems().contains(item));
-        assertEquals(allyUnit.getItemAmount(item.getName()), 0);
+        assertTrue("Unit does not have 'item' in its set",!allyUnit.getItems().contains(item1));
+        assertEquals(allyUnit.getItemAmount(item1.getName()), 0);
         assertEquals(allyUnit.getItem("item"), null);
     }
 
