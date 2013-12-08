137fe612f7c687d736e0e6f90f04df2d84ebd006
Kevin Jian <kjian279@gmail.com>
2013-12-08 10:46:43 -0500

    <triv> minor formatting and documenting

--- src/controllers/GameManager.java
+++ src/controllers/GameManager.java
@@ -100,8 +100,7 @@ public class GameManager extends Manager {
      * Makes a new AI and calls the AI doTurn method to execute AI
      */
     public void doAITurn () {
-//        AI2 ai = new AI2(myActiveStage.getTeam(myActiveTeam), myActiveStage, this);
-        AI ai = new AI(myActiveStage.getTeam(myActiveTeam), myActiveStage, this);
+        AI2 ai = new AI2(myActiveStage.getTeam(myActiveTeam), myActiveStage, this);
         ai.doTurn();
     }
 
@@ -137,7 +136,7 @@ public class GameManager extends Manager {
     }
 
     private void setActiveActions (Coordinate coordinate) {
-        List<String> myActiveActionNames = getActions(coordinate);
+        List<String> myActiveActionNames = getActionNames(coordinate);
         if (myActiveActionNames != null) {
             List<Action> newActiveActions = new ArrayList<>();
 
