71fc8aa39ffc744d634b00da240f481c6cb92f03
Brooks Mershon <brooksmershon@gmail.com>
2013-12-08 15:11:06 -0500

    farm resources

---
+++ src/dialog/dialogs/GameJTable.java
@@ -0,0 +1,30 @@
+package dialog.dialogs;
+
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+import dialog.dialogs.tableModels.GameTableModel;
+
+/**
+ * 
+ * @author Leevi, brooksmershon
+ *
+ */
+
+@SuppressWarnings("serial")
+public class GameJTable extends JTable {
+    
+    /**
+     * GameTableModel passed in to control JTable
+     * @param gtm
+     */
+    public GameJTable (GameTableModel gtm) {
+        super(gtm);
+    }
+
+    @Override
+    public TableCellEditor getCellEditor (int row, int column) {
+        Object value = getValueAt(row, column);
+        if (value != null) { return getDefaultEditor(value.getClass()); }
+        return super.getCellEditor(row, column);
+    }
+}
